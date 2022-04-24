c447927e353c4d0a13169975a56cdb605d1ac336
LTH4823 <peter4823zx@naver.com>
2022-04-24 13:43:20 +0900

    add Advice

---
+++ src/main/java/org/zerock/boardtest/controller/exception/BasicControllerAdvice.java
@@ -0,0 +1,41 @@
+package org.zerock.boardtest.controller.exception;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.util.Arrays;
+
+@ControllerAdvice
+@Log4j2
+//코드 오류 발생 시 처리
+public class BasicControllerAdvice {
+    @ResponseBody
+    @ExceptionHandler(Exception.class)
+    public String exceptCommon(Exception exception){
+
+        log.error("-----------------------------------");
+        log.error(exception.getMessage());
+
+        StringBuffer buffer = new StringBuffer("<ul>");
+
+        buffer.append("<li>" +exception.getMessage()+"</li>");
+
+        Arrays.stream(exception.getStackTrace()).forEach(stackTraceElement -> {
+            buffer.append("<li>"+stackTraceElement+"</li>");
+        });
+        buffer.append("</ul>");
+
+        return buffer.toString();
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String notFound(){
+        return "custom404";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/custom404.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <h1>404 page 내용 없음</h1>
+</body>
+</html>
